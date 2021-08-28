11347adc1e04cc7df3f477668b7ba03cb0c39ce9
Laus <lausvan1205@gmail.com>
2021-08-27 21:43:19 -0300

    Tela de login conectada ao backend

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,21 +1,62 @@
-import React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, AsyncStorage,  } from 'react-native';
 
 import InputText from '../../components/forms/InputText';
 import Button from '../../components/forms/button';
+import api from '../../services/api';
 
 const Login: React.FC<any> = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user => {
+      if (user) {
+        navigation.navigate('Menu');
+      }
+    });
+  }, []);
+
+  const handleLogin = async () => {
+    const response = await api.post('/user/authenticate', {
+      email,
+      password
+    }).catch(error => {
+      if (error) {
+        alert('Usuário não encontrado!');
+      }
+    });
+
+    if (response) {
+      await AsyncStorage.setItem('user', response.data.jwt);
+      navigation.navigate('Menu')
+    }
+  }
+
+
   return (
     <View style={styles.container}>
       <View style={styles.photoContainer}>
         <Text style={styles.question}>?</Text>
       </View>
       <View style={styles.inputsContainer}>
-        <InputText icon="envelope" placeholder="E-mail" autoCorrect={false} />
-        <InputText icon="lock" placeholder="Senha" autoCorrect={false} />
+        <InputText 
+          icon="envelope" 
+          placeholder="E-mail" 
+          autoCapitalize="none"
+          autoCorrect={false} 
+          keyboardType="email-address" 
+          value={email} 
+          onChangeText={setEmail} />
+        <InputText 
+          icon="lock" 
+          placeholder="Senha"
+          autoCorrect={false} 
+          value={password} 
+          onChangeText={setPassword} />
       </View>
       <View style={styles.buttonConteiner}>
-        <Button title="Logar" onPress={() => { navigation.navigate('Menu') }} />
+        <Button title="Logar" onPress={handleLogin} />
       </View>
       <View style={styles.createAccountContainer}>
         <Text style={styles.labelColor}>Não possui uma conta?</Text>
